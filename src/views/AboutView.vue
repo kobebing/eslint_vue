362bf01028808c9396beafe9d6220c0a9d0e4f9c
bing.zhang <bing.zhang@pakpobox.com>
2024-04-09 22:00:00 +0800

    eslint项目上传

---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script setup>
+import HomeView from "./HomeView.vue";
+import { ref } from "vue";
+
+let test = ref("");
+
+const fun = () => {
+  if (true) {
+    return;
+  }
+  let a = 1;
+  let b = 2;
+  console.log(a + b);
+};
+</script>
